356a2d67b2287c1f97a7c34d1cd62252bfca98b7
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-04-16 07:52:54 -0300

    Feat: configuração parcial do github pages

--- src/Router.js
+++ src/Router.js
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 function Routers() {
   return (
     <Routes>
-      <Route exact path="/" element={ <Home /> } />
+      <Route exact path="/unidunite" element={ <Home /> } />
     </Routes>
   )
 }
