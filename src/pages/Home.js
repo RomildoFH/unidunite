5bf532b19b012798ae4dd72e46edb94bf48e93cf
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-04-16 07:15:15 -0300

    Feat: criando campos provisórios para configuração geral do projeto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="page-container">
+      <h1>Uni Duni Tê Kids</h1>
+      <h4>Nossa diversão é levar estilo e conforto para seu pequeno</h4>
+      <label>
+        e-mail
+        <input type="text" />
+      </label>      
+      <button>Eu sou um botão</button>
+    </div>
   )
 }
 
